188e86d3bbe16561b24d3879ec8459b6cabe7c3a
Yousif <yousifhazardous@gmail.com>
2024-08-28 11:43:31 +0300

    Add printable PDF template for properties

--- models/res_users.py
+++ models/res_users.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class UsersInherited(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate.property", "seller", string="Properties", domain=['|', ('state', '=', 'new'), ('state', '=', 'offer_received')])
+    property_ids = fields.One2many("estate.property", "seller", string="Properties")
